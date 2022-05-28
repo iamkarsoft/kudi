1ecf8632963a13261534b70e6aaf53a80139bc05
Kofi Romain Amoussou <karsoft92@gmail.com>
2022-05-28 13:54:34 +0000

    ignore folder

--- src/Facades/Kudi.php
+++ src/Facades/Kudi.php
@@ -1,10 +1,14 @@
-<?php 
+<?php
 
 namespace Iamkarsoft\Kudi\Facades;
+
 use Illuminate\Support\Facades\Facade;
 
-class Kudi extends Facade{
-    protected static function getFacadeAccessor(){
+class Kudi extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        // return \Iamkarsoft\Kudi\Kudi::class;
         return  'kudi';
     }
 }
